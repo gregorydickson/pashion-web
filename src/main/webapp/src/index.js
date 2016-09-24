37c8904dc661425bde696e2073a1aebeb529d43b
dickmoonpie <mr.richard.moon@gmail.com>
2016-09-24 12:50:58 -0400

    Basic Basic movement of the Accordian
    Really basic view of how accordions could start to work. Test of
    working model RM / GD

--- src/main/webapp/src/index.js
+++ src/main/webapp/src/index.js
@@ -1,2 +1,10 @@
 export class Index {
+
+closeExpand() {
+  var buttonChoice = document.getElementById("button1");
+  var panelChoice = document.getElementById("panel1");
+  buttonChoice.classList.toggle("active");
+  panelChoice.classList.toggle("show");
+}
+
 }
