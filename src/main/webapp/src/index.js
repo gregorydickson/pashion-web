3d7c6e1a83b2b539ca8f742decfd3805d2a307f8
dickmoonpie <mr.richard.moon@gmail.com>
2016-09-28 14:35:17 -0400

    Merge look (js and html) into index to fix flex grids

--- src/main/webapp/src/index.js
+++ src/main/webapp/src/index.js
@@ -1,11 +1,60 @@
-export class Index {
+import {inject} from 'aurelia-framework';
+import {HttpClient} from 'aurelia-fetch-client';
+import 'fetch';
 
-/* RM button */
+@inject(HttpClient)
+export class Index {
+  heading = 'Looks for a Collection';
+  looks = [];
+  seasons = [];
+  brands = [];
+  searchText = '';
+
+
+  constructor(http) {
+    http.configure(config => {
+      config
+        .useStandardConfiguration();
+    });
+    this.http = http;
+    this.boundHandler = this.handleKeyInput.bind(this);
+  }
+
+  activate() {
+    window.addEventListener('keypress', this.boundHandler, false);
+    return Promise.all([
+      this.http.fetch('/brandCollection/looks/1').then(response => response.json()).then(collection => this.looks = collection.looks),
+      this.http.fetch('/brandCollection/seasons').then(response => response.json()).then(seasons => this.seasons = seasons),
+      this.http.fetch('/brand/index.json').then(response => response.json()).then(brands => this.brands = brands)
+
+    ]);
+  }
+
+  deactivate() {
+   window.removeEventListener('keypress', this.boundHandler);
+  }
+
+  handleKeyInput(event) {
+    console.log(event);
+    if(event.which == 13 && event.srcElement.id === "search") {
+      console.log("herow");
+      this.http.fetch('/look/search?searchtext='+ encodeURI(event.srcElement.value))
+          .then(response => response.json())
+          .then(looks => {
+              this.looks = looks        
+          })
+
+    }
+  }
+ 
+ /* RM accordion expansion button */
 closeExpand(buttonNumber) {
   var buttonChoice = document.getElementById("button" + buttonNumber);
   var panelChoice = document.getElementById("panel" + buttonNumber);
   buttonChoice.classList.toggle("active");
   panelChoice.classList.toggle("show");  
 }
-
+    
 }
+
+
--- src/main/webapp/src/looks.js
+++ src/main/webapp/src/looks.js
@@ -1,50 +1,5 @@
-import {inject} from 'aurelia-framework';
-import {HttpClient} from 'aurelia-fetch-client';
-import 'fetch';
 
-@inject(HttpClient)
 export class Looks {
-  heading = 'Looks for a Collection';
-  looks = [];
-  seasons = [];
-  brands = [];
-  searchText = '';
 
-
-  constructor(http) {
-    http.configure(config => {
-      config
-        .useStandardConfiguration();
-    });
-    this.http = http;
-    this.boundHandler = this.handleKeyInput.bind(this);
-  }
-
-  activate() {
-    window.addEventListener('keypress', this.boundHandler, false);
-    return Promise.all([
-      this.http.fetch('/brandCollection/looks/1').then(response => response.json()).then(collection => this.looks = collection.looks),
-      this.http.fetch('/brandCollection/seasons').then(response => response.json()).then(seasons => this.seasons = seasons),
-      this.http.fetch('/brand/index.json').then(response => response.json()).then(brands => this.brands = brands)
-
-    ]);
-  }
-
-  deactivate() {
-   window.removeEventListener('keypress', this.boundHandler);
-  }
-
-  handleKeyInput(event) {
-    console.log(event);
-    if(event.which == 13 && event.srcElement.id === "search") {
-      console.log("herow");
-      this.http.fetch('/look/search?searchtext='+ encodeURI(event.srcElement.value))
-          .then(response => response.json())
-          .then(looks => {
-              this.looks = looks        
-          })
-
-    }
-  }
     
 }
