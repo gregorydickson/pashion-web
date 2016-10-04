c77653adeef92d9c1e1de5cd82b2ceebbaefd458
dickmoonpie <mr.richard.moon@gmail.com>
2016-10-04 17:49:07 -0400

    Match request man calendar format with small request calendars

--- src/main/webapp/src/requestman.js
+++ src/main/webapp/src/requestman.js
@@ -1,3 +1,11 @@
 export class Requestman{
-	
+	  /* RM accordion expansion button */
+  closeExpand(buttonNumber) {
+    var buttonChoice = document.getElementById("button" + buttonNumber);
+    var panelChoice = document.getElementById("panel" + buttonNumber);
+    buttonChoice.classList.toggle("active");
+    panelChoice.classList.toggle("show");  
+  }
+    
+
 }
