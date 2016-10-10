10659f3901fd408a6c1432112e8d488fdc249b46
Gregg Dickson <gregory.d.dickson@gmail.com>
2016-10-10 16:18:07 -0500

    calendar in create sample request

--- grails-app/controllers/pashion/CalendarController.groovy
+++ grails-app/controllers/pashion/CalendarController.groovy
@@ -22,4 +22,27 @@ class CalendarController {
 
     	render aCalendar as JSON
     }
+
+    def searchableItemPicker(){
+        log.info "item:"+params.item
+
+        log.info "locale:"+request.locale
+        log.info "offset:"+params.offset
+        log.info "day:"+params.day
+        log.info "month:"+params.month
+        log.info "year:"+params.year
+        SearchableItem theItem = SearchableItem.get(params.item)
+
+        Map events = [:]
+        def aCalendar = new PashionCalendar( events,
+                                             params.month.toInteger(),
+                                             params.day.toInteger(),
+                                             params.year.toInteger(),
+                                             request.locale.toString(),
+                                             params.offset.toInteger(),
+                                             params.months.toInteger())
+
+        render aCalendar as JSON
+
+    }
 }
--- src/main/webapp/src/sample_request/create.js
+++ src/main/webapp/src/sample_request/create.js
@@ -2,11 +2,16 @@ import {DialogController} from 'aurelia-dialog';
 import {HttpClient} from 'aurelia-fetch-client';
 import 'fetch';
 import {inject} from 'aurelia-framework';
+import {DateFormat} from 'common/dateFormat';
 
 @inject(HttpClient, DialogController)
 export class Create {
   static inject = [DialogController];
   currentItem = {};
+  startCalendar = {};
+  endCalendar = {};
+  startOffset = 0;
+  endOffset = 0;
 
   constructor(http, controller){
     this.controller = controller;
@@ -19,6 +24,76 @@ export class Create {
   activate(itemId){
   	this.http.fetch('/searchableItems/' +itemId+ '.json')
       .then(response => response.json()).then(item => this.currentItem = item);
+
+    var queryString = DateFormat.urlString(0, 2);
+    this.http.fetch('/calendar/searchableItemPicker' +queryString+ '&item='+itemId)
+    	.then(response => response.json())
+        .then(calendar => {
+              this.startCalendar = calendar;
+              this.endCalendar = calendar;
+          });
     
+  }
+
+  startNext(){
+  	var queryString = DateFormat.urlString(++this.startOffset,1);
+    return this.http.fetch('/calendar/searchableItemPicker' + queryString+
+    					   '&item='+this.currentItem.id)
+          .then(response => response.json())
+          .then(calendar => {
+              this.startCalendar = calendar;
+          })
+
+  }
+  startPrevious(){
+  	var queryString = DateFormat.urlString(--this.startOffset,1);
+    return this.http.fetch('/calendar/searchableItemPicker' +queryString+
+    					   '&item='+this.currentItem.id)
+          .then(response => response.json())
+          .then(calendar => {
+              this.startCalendar = calendar;
+          })
+
+  }
+  startReset(){
+  	var queryString = DateFormat.urlString(0,1);
+    return this.http.fetch('/calendar/searchableItemPicker' + queryString+
+    					   '&item='+this.currentItem.id)
+          .then(response => response.json())
+          .then(calendar => {
+              this.startCalendar = calendar;
+          })
+
+  }
+
+  endNext(){
+  	var queryString = DateFormat.urlString(++this.endOffset,1);
+    return this.http.fetch('/calendar/searchableItemPicker' + queryString+
+    					   '&item='+this.currentItem.id)
+          .then(response => response.json())
+          .then(calendar => {
+              this.endCalendar = calendar;
+          })
+
+  }
+  endPrevious(){
+  	var queryString = DateFormat.urlString(--this.endOffset,1);
+    return this.http.fetch('/calendar/searchableItemPicker' +queryString+
+    					   '&item='+this.currentItem.id)
+          .then(response => response.json())
+          .then(calendar => {
+              this.endCalendar = calendar;
+          })
+
+  }
+  endReset(){
+  	var queryString = DateFormat.urlString(0,1);
+    return this.http.fetch('/calendar/searchableItemPicker' + queryString+
+    					   '&item='+this.currentItem.id)
+          .then(response => response.json())
+          .then(calendar => {
+              this.endCalendar = calendar;
+          })
+
   }
 }
