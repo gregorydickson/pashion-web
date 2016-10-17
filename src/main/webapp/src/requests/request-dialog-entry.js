4a129531fe797a180948c4c2b886fa7e007a856e
dickmoonpie <mr.richard.moon@gmail.com>
2016-10-17 13:37:22 -0400

    Formatting of the request sample dialog

---
+++ src/main/webapp/src/requests/request-dialog-entry.js
@@ -0,0 +1,21 @@
+import {DialogController} from 'aurelia-dialog';
+import {HttpClient} from 'aurelia-fetch-client';
+import 'fetch';
+import {inject} from 'aurelia-framework';
+import {DateFormat} from 'common/dateFormat';
+
+@inject(HttpClient, DialogController)
+export class RequestDialogEntry {
+	static inject = [DialogController];
+
+
+  constructor(http, controller){
+    this.controller = controller;
+    http.configure(config => {
+      config
+        .useStandardConfiguration();
+    });
+    this.http = http;
+  }
+
+  }
