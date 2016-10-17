2fd9fbcd21c3fbf3ddb9f61c3798e0ad877b3995
dickmoonpie <mr.richard.moon@gmail.com>
2016-10-17 16:44:04 -0400

    typo

--- src/main/webapp/src/edit_request/create-edit-dialog-entry.js
+++ src/main/webapp/src/edit_request/create-edit-dialog-entry.js
@@ -5,7 +5,7 @@ import {inject} from 'aurelia-framework';
 import {DateFormat} from 'common/dateFormat';
 
 @inject(HttpClient, DialogController)
-export class CreateEditDialog {
+export class CreateEditDialogEntry {
 	static inject = [DialogController];
 
 
