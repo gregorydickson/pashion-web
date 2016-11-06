0a3314784bc00d952fa8e4038e913c8d903963e9
OlgaYellowflowers <olga.yellowflowers@gmail.com>
2016-11-06 15:09:21 +0200

    #18 Made EDIT CONTACT dialog

--- src/main/webapp/src/comms/contactEntry.js
+++ src/main/webapp/src/comms/contactEntry.js
@@ -1,4 +1,42 @@
-export class ContactEntry {
+import {HttpClient} from 'aurelia-fetch-client';
+import {DialogController} from 'aurelia-dialog';
+import {DialogService} from 'aurelia-dialog';
+import 'fetch';
+import {inject} from 'aurelia-framework';
+import {DateFormat} from 'common/dateFormat';
+import {CreateDialogEditContact} from './../edit_request/create-dialog-edit-contact';
 
-    
+@inject(HttpClient, DialogController, DialogService)
+export class ContactEntry {
+	static inject = [DialogController];
+
+
+  constructor(http, controller, dialogService){
+    this.controller = controller;
+    http.configure(config => {
+      config
+        .useStandardConfiguration();
+    });
+    this.http = http;
+    this.dialogService = dialogService;
+
+  }
+
+  // Three dots contact edit menu
+
+  lookEditContact(id){
+    var menu = document.getElementById("lookEditContact"+id);
+    menu.classList.toggle("look-menu-show");
+  }
+
+  // Create dialog edit contact 
+
+  createDialogEditContact(itemId) {
+    var menu = document.getElementById("lookEditContact"+itemId);
+    menu.classList.toggle("look-menu-show");
+    this.dialogService.open({viewModel: CreateDialogEditContact, model: itemId })
+      .then(response => {});
+  }
+
+
 }
--- src/main/webapp/src/comms/contacts.js
+++ src/main/webapp/src/comms/contacts.js
@@ -1,4 +1,16 @@
+import {inject} from 'aurelia-framework';
+import {HttpClient} from 'aurelia-fetch-client';
+import {DialogService} from 'aurelia-dialog';
+import 'fetch';
+
+@inject(HttpClient, DialogService)
 export class Contacts {
 
-    
+  // Three dots contact edit menu
+
+  lookEditContact(id){
+    var menu = document.getElementById("lookEditContact"+id);
+    menu.classList.toggle("look-menu-show");
+  }
+
 }
