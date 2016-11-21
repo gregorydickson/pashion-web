9a48a388038797a18a30534269fb24b6e2865312
OlgaYellowflowers <olga.yellowflowers@gmail.com>
2016-11-21 16:28:39 +0200

    #26 Made update your photo dialog

--- src/main/webapp/src/contacts/contactEntry.js
+++ src/main/webapp/src/contacts/contactEntry.js
@@ -5,6 +5,7 @@ import 'fetch';
 import {inject} from 'aurelia-framework';
 import {DateFormat} from 'common/dateFormat';
 import {CreateDialogEditContact} from './dialogEditContact';
+import {CreateDialogUpdatePhoto} from './dialogUpdatePhoto';
 
 @inject(HttpClient, DialogController, DialogService)
 export class ContactEntry {
@@ -35,6 +36,16 @@ export class ContactEntry {
     var menu = document.getElementById("lookEditContact"+itemId);
     menu.classList.toggle("look-menu-show");
     this.dialogService.open({viewModel: CreateDialogEditContact, model: itemId })
+      .then(response => {});
+  }
+
+
+  // Create dialog update photo
+
+  CreateDialogUpdatePhoto(itemId) {
+    var menu = document.getElementById("lookEditContact"+itemId);
+    menu.classList.toggle("look-menu-show");
+    this.dialogService.open({viewModel: CreateDialogUpdatePhoto, model: itemId })
       .then(response => {});
   }
 
---
+++ src/main/webapp/src/contacts/dialogUpdatePhoto.js
@@ -0,0 +1,68 @@
+import {DialogController} from 'aurelia-dialog';
+import {HttpClient,json} from 'aurelia-fetch-client';
+import 'fetch';
+import {inject} from 'aurelia-framework';
+import {DateFormat} from 'common/dateFormat';
+
+@inject(HttpClient, DialogController)
+export class CreateDialogUpdatePhoto {
+  static inject = [DialogController];
+
+  constructor(http, controller){
+    this.controller = controller;
+
+    http.configure(config => {
+      config
+        .useStandardConfiguration();
+    });
+    this.http = http;
+  }
+
+  activate(itemId){
+    this.http.fetch('/searchableItems/'+itemId+'.json')
+         .then(response => response.json())
+         .then(item => {}
+         );
+  }
+
+  close(){
+    this.controller.close();
+  }
+
+  attached() {
+ 
+    var inputs = document.querySelectorAll( '.input-file' );
+    Array.prototype.forEach.call( inputs, function(input) {
+      var label  = input.nextElementSibling,
+        labelVal = label.innerHTML;
+
+        // Fit width of file input and label
+        input.style.width = label.offsetWidth + "px";
+
+        input.addEventListener( 'change', function(e) {
+
+          // Add to styled file input count feature
+            var fileName = '';
+            if( this.files && this.files.length > 1 )
+              fileName = ( this.getAttribute( 'data-multiple-caption' ) || '' ).replace( '{count}', this.files.length );
+            else
+              fileName = e.target.value.split( '\\' ).pop();
+
+            if( fileName )
+              label.querySelector( 'span' ).innerHTML = fileName;
+            else
+              label.innerHTML = labelVal;
+
+          // Make width of file input and label the same
+            input.style.width = label.offsetWidth + "px";
+    
+        });
+
+
+    });
+
+
+
+  }
+  
+}
