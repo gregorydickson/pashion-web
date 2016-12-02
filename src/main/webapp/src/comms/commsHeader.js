d328da8ffea8e73d236be1237721b7bd06bd20ee
dickmoonpie <mr.richard.moon@gmail.com>
2016-12-02 18:34:20 -0500

    First letter avatars in comms panel

--- src/main/webapp/src/comms/commsHeader.js
+++ src/main/webapp/src/comms/commsHeader.js
@@ -86,6 +86,8 @@ export class CommsHeader {
       // Scroll messages to the end
       if (this.comms.status == this.statusValues.messages)
         window.setTimeout(function () {
+
+              $("#right-panel-body").height($("#right-panel-body").height()+500); // RM kludge to redraw flex box with new elements
               $("#right-panel-body").scrollTop($("#right-panel-body").prop("scrollHeight"));
             },1000); // major kludge to scroll messages
 
