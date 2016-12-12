4080ab4b91e1cf5e14bfa4862ce59133582e8699
Gregg Dickson <gregory.d.dickson@gmail.com>
2016-12-12 14:19:58 -0600

    remove resize function on zoom close

--- src/main/webapp/src/zoom/zoom.js
+++ src/main/webapp/src/zoom/zoom.js
@@ -1,6 +1,7 @@
 import {DialogController} from 'aurelia-dialog';
 import {inject} from 'aurelia-framework';
 import {DateFormat} from 'common/dateFormat';
+import $ from 'jquery';
 
 
 @inject(DialogController)
@@ -39,39 +40,22 @@ export class Zoom {
     this.controller.close();
   }
 
+  detached(){
+    console.log("detached");
+    $(window).off("resize", zoomSize);
+     //window.removeEventListener("resize", zoomSize(), true);
+  }
+
   attached() {
  
 
     // Fit image to screen height
 
-    function zoomSize() {
-        var windowHeight = window.innerHeight;
-        var windowWidth = window.innerWidth;                
-        var imageHeight = document.getElementById('imageZoom').clientHeight;
-        var imageWidth = document.getElementById('imageZoom').clientWidth;
-        
-        document.getElementById('imageZoom').style.maxHeight= 'none';     
-
-        if ( imageHeight < windowHeight*0.7 ) {
-          document.getElementById('imageZoom').style.maxHeight= windowHeight*0.75 +'px';     
-          document.getElementById('imageZoom').style.height= 'auto';  
-        } 
-        else {
-          document.getElementById('imageZoom').style.height= windowHeight*0.75 +'px';     
-         
-        }   
-
-        if ( imageWidth > windowWidth*0.7) {
-          document.getElementById('imageZoom').style.maxHeight= windowHeight*0.75 +'px';     
-          document.getElementById('imageZoom').style.height= 'auto';  
-        }       
-    }
+    
 
     zoomSize();
 
-    window.addEventListener("resize", function(){
-      zoomSize ();
-    }, true);
+    window.addEventListener("resize", zoomSize, true);
 
     // Close dialog when click outside
     var controller = this.controller;
@@ -154,3 +138,30 @@ export class Zoom {
   }
   
 }
+
+var zoomSize = function() {
+      try{
+        var windowHeight = window.innerHeight;
+        var windowWidth = window.innerWidth;                
+        var imageHeight = document.getElementById('imageZoom').clientHeight;
+        var imageWidth = document.getElementById('imageZoom').clientWidth;
+        
+        document.getElementById('imageZoom').style.maxHeight= 'none';     
+
+        if ( imageHeight < windowHeight*0.7 ) {
+          document.getElementById('imageZoom').style.maxHeight= windowHeight*0.75 +'px';     
+          document.getElementById('imageZoom').style.height= 'auto';  
+        } 
+        else {
+          document.getElementById('imageZoom').style.height= windowHeight*0.75 +'px';     
+         
+        }   
+
+        if ( imageWidth > windowWidth*0.7) {
+          document.getElementById('imageZoom').style.maxHeight= windowHeight*0.75 +'px';     
+          document.getElementById('imageZoom').style.height= 'auto';  
+        } 
+      } catch(e){
+        console.error("Error in Zoom resize");
+      }    
+    }
