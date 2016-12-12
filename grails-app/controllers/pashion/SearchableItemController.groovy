f8ea592283ff19550c5c2c24a9d74ed2410e51c4
Gregg Dickson <gregory.d.dickson@gmail.com>
2016-12-12 10:52:07 -0600

    five images in image search index.html and guestpage.html

--- grails-app/controllers/pashion/SearchableItemController.groovy
+++ grails-app/controllers/pashion/SearchableItemController.groovy
@@ -82,7 +82,7 @@ class SearchableItemController {
                 }
             } 
         
-        def fixImagesPerRow = 3 
+        def fixImagesPerRow = 5 
         if(fixImagesPerRow > 5) fixImagesPerRow = 5
         if(fixImagesPerRow < 3) fixImagesPerRow = 3
         log.info "Images per row: "+fixImagesPerRow
