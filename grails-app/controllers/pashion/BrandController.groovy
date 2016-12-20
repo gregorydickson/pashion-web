4d1ce3536cd4b587283ac7f60f609d7c646d77c0
Gregg Dickson <gregory.d.dickson@gmail.com>
2016-12-20 09:57:09 -0600

    remove debugging code from Brand Controller

--- grails-app/controllers/pashion/BrandController.groovy
+++ grails-app/controllers/pashion/BrandController.groovy
@@ -10,11 +10,7 @@ class BrandController {
     static allowedMethods = [save: "POST", update: "PUT", delete: "DELETE"]
 
     def index(Integer max) {
-        long startTime = System.currentTimeMillis()
         respond Brand.list(), model:[brandCount: Brand.count()]
-        long endTime = System.currentTimeMillis()
-        long duration = (endTime - startTime)
-        log.info "brand controller:"+duration
     }
 
     def fastList(){
