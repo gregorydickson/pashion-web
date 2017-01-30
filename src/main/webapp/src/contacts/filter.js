2ec41ae1630392654383be0e17d2216505f1c61b
dickmoonpie <mr.richard.moon@gmail.com>
2017-01-29 21:54:52 -0500

    checks on filtering and catch

--- src/main/webapp/src/contacts/filter.js
+++ src/main/webapp/src/contacts/filter.js
@@ -1,11 +1,21 @@
 export class FilterValueConverter {
-  toView(array, searchTerm, filterFunc) {
-	
-    return array.filter((item) => {
-	
-	  let matches = searchTerm && searchTerm.length > 0 ? filterFunc(searchTerm,item): true;
-	  				
-	  return matches;
-    });
-  }
+    toView(array, searchTerm, filterFunc) {
+
+        if (!typeof(array)) return;
+        if (!array) return;
+        if (array == null) return;
+        if (array == 'undefined') return;
+        if (array == undefined) return;
+
+
+        try {
+            return array.filter((item) => {
+
+                let matches = searchTerm && searchTerm.length > 0 ? filterFunc(searchTerm, item) : true;
+
+                return matches;
+            });
+        } catch (e) {}
+
+    }
 }
