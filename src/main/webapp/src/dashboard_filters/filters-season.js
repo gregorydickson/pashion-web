0f4a1347c36c7cf95910952098a18eb8435fb38a
dickmoonpie <mr.richard.moon@gmail.com>
2017-02-15 22:19:05 -0500

    Add seasons sorting to season filters

--- src/main/webapp/src/dashboard_filters/filters-season.js
+++ src/main/webapp/src/dashboard_filters/filters-season.js
@@ -20,9 +20,22 @@ setSeason(event){
 	this.seasono = event.detail.value;
 }
 
+
+sortSeasons (array) {
+
+    var iarray = array.slice(0)
+        .sort(
+          function (a, b) {
+          //console.log (a["order"] + " " + b["order"] + " " + (a["order"] - b["order"] ));
+          return (a["order"] - b["order"]) * -1  }
+          );
+    return iarray;
+  } 
+
+
 attached() {
     return Promise.all([
-      this.http.fetch('/dashboard/seasonsByBrand').then(response => response.json()).then(seasons => this.seasons = seasons)
+      this.http.fetch('/dashboard/seasonsByBrand').then(response => response.json()).then(seasons => this.seasons = this.sortSeasons(seasons))
     ]);
 }
 
