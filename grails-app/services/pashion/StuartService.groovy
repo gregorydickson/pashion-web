2016bfb30abcba9af1570f6e3ce4b573b655d833
Gregg Dickson <gregory.d.dickson@gmail.com>
2017-03-21 17:20:06 -0500

    stuart service job quote and create job

--- grails-app/services/pashion/StuartService.groovy
+++ grails-app/services/pashion/StuartService.groovy
@@ -42,7 +42,7 @@ class StuartService {
 		newToken
 	}
 
-
+	// Recommended that we do not use this, just do a Job Quote
 	def createLocation(Address address, String placeTypeId){
 		if(token == null)
 			token = KeyValue.findByItemKey("stuart")?.itemValue
@@ -75,28 +75,73 @@ class StuartService {
 		newLocation
 	}
 
-	
+	/*
+		Transport Type IDs (available in Paris and London)
+		2	Bike	Maximum capacity of 80L (60cm x 40cm x 26cm) and 12kg
+		3	Motorbike	Maximum capacity of 80L (60cm x 40cm x 26cm) and 15kg
+		4	Car	Maximum capacity of 200L (50cm x 80cm x 50cm) and 50kg
+		5	Cargo Bike	Electric engine-supported cargo-bikes with maximum capacity of 140L (46cm x 47cm x 68cm) and 50kg
+		
+		https://docs.stuart.com/#/reference/job-quotes/create-a-job-quote
+
+	*/
 	def createJobQuote(Address fromAddress, Address toAddress){
 		if(token == null)
 			token = KeyValue.findByItemKey("stuart")?.itemValue
 		if(token == null)
 			newToken()
 
+		def destination = toAddress.address1 + " " + toAddress.postalCode + " " + toAddress.city
+		def origin = fromAddress.address1 + " " + fromAddress.postalCode + " " + fromAddress.city
 		def quote = http.post {
 			request.headers['Authorization'] = 'Bearer ' + token
     		request.uri.path = '/v1/jobs/quotes/types'
     		request.contentType = 'application/x-www-form-urlencoded'
-    		request.body = []
+    		request.body = [
+    				transportTypeIds:"2,3",
+					destination:destination,
+					destinationContactCompany:fromAddress.company,
+					destinationContactFirstname:fromAddress.name.split()[0],
+					destinationContactLastname:fromAddress.name.split()[1],
+					destinationContactPhone:fromAddress.contactPhone,
+					origin:origin,
+					originContactPhone:toAddress.contactPhone]
     		response.success { FromServer from, Object body ->
-    			
     			log.info "body:"+body
         		return body
     		}
 		}
 		quote
+	}
 
 
+	def createJob(String quoteId, Address fromAddress,
+					 Address toAddress, SampleRequest sampleRequest){
 
+		if(token == null)
+			token = KeyValue.findByItemKey("stuart")?.itemValue
+		if(token == null)
+			newToken()
+
+		def destination = toAddress.address1 + " " + toAddress.postalCode + " " + toAddress.city
+		def origin = fromAddress.address1 + " " + fromAddress.postalCode + " " + fromAddress.city
+		def job = http.post {
+			request.headers['Authorization'] = 'Bearer ' + token
+    		request.uri.path = '/v1/jobs'
+    		request.contentType = 'application/x-www-form-urlencoded'
+    		request.body = [
+    				jobQuoteId:quoteId,
+    				clientInvoiceReference:sampleRequest.id,
+    				destinationComment:toAddress.comment,
+    				originComment:fromAddress.comment,
+    				pickupAt:sampleRequest.pickupDate.format("yyyy-MM-dd'T'HH:mm:ss'Z'", TimeZone.getTimeZone("UTC"))
+    				]
+    		response.success { FromServer from, Object body ->
+    			log.info "body:"+body
+        		return body
+    		}
+		}
+		job
 	}
 
 }
