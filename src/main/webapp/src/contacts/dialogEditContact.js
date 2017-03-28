0876fb565306484861fe62f45a7a64f4a6638721
dickmoonpie <mr.richard.moon@gmail.com>
2017-03-28 15:36:08 -0400

    Change dialogEditContact to close user so that changes are not immediately bound. close the value
    Copies a clone function from here:
    http://stackoverflow.com/questions/728360/how-do-i-correctly-clone-a-jav
    ascript-object?page=1&tab=votes#tab-top
    Also remove getUserDetails

--- src/main/webapp/src/contacts/dialogEditContact.js
+++ src/main/webapp/src/contacts/dialogEditContact.js
@@ -12,6 +12,8 @@ import { PRAgencyService } from 'services/PRAgencyService';
 export class CreateDialogEditContact {
     static inject = [DialogController];
 
+    lUser = {};
+
     constructor(http, controller, userService, brandService, prAgencyService, pressHouseService) {
         this.controller = controller;
         http.configure(config => {
@@ -30,26 +32,27 @@ export class CreateDialogEditContact {
     activate() {
         this.userService.getUser()
             .then(user => {
-                this.userService.getUserDetails(user.id)
-                    .then(currentContact => {
-                        this.user = currentContact;
-                        if (this.user.brand != null)
-                            this.brandService.getBrandAddresses(this.user.brand.id)
+                //this.lUser = user;
+
+                
+                this.lUser = clone(user);
+
+                        if (this.lUser.brand != null)
+                            this.brandService.getBrandAddresses(this.lUser.brand.id)
                                 .then(addresses=>this.addresses = addresses)
-                        if (this.user.pressHouse != null)
-                            this.pressHouseService.getPressHouseAddresses(this.user.pressHouse.id)
+                        if (this.lUser.pressHouse != null)
+                            this.pressHouseService.getPressHouseAddresses(this.lUser.pressHouse.id)
                                 .then(addresses=>this.addresses = addresses)
-                        if (this.user.prAgency != null)
-                            this.prAgencyService.getPRAgencyAddresses(this.user.PRAgency.id)
+                        if (this.lUser.prAgency != null)
+                            this.prAgencyService.getPRAgencyAddresses(this.lUser.PRAgency.id)
                                 .then(addresses=>this.addresses = addresses)
 
                       })
-                })
         }
 
    save(){
-    console.log("CreateDialogEditContact.save, updating user:"+this.user.id + " name: " + this.user.name)
-    this.userService.update(this.user);
+    console.log("CreateDialogEditContact.save, updating user:"+this.lUser.id + " name: " + this.lUser.name)
+    this.userService.update(this.lUser);
     this.controller.close();
    }
 
@@ -59,4 +62,39 @@ export class CreateDialogEditContact {
 
 
 
+}
+
+
+function clone(obj) {
+    var copy;
+
+    // Handle the 3 simple types, and null or undefined
+    if (null == obj || "object" != typeof obj) return obj;
+
+    // Handle Date
+    if (obj instanceof Date) {
+        copy = new Date();
+        copy.setTime(obj.getTime());
+        return copy;
+    }
+
+    // Handle Array
+    if (obj instanceof Array) {
+        copy = [];
+        for (var i = 0, len = obj.length; i < len; i++) {
+            copy[i] = clone(obj[i]);
+        }
+        return copy;
+    }
+
+    // Handle Object
+    if (obj instanceof Object) {
+        copy = {};
+        for (var attr in obj) {
+            if (obj.hasOwnProperty(attr)) copy[attr] = clone(obj[attr]);
+        }
+        return copy;
+    }
+
+    throw new Error("Unable to copy obj! Its type isn't supported.");
 }
