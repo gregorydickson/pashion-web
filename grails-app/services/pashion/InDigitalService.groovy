34dd555b1c0873ba40d2fc886ac2a094a966c675
Gregg Dickson <gregory.d.dickson@gmail.com>
2017-04-04 12:54:36 -0500

    Test of InDigital API #268

--- grails-app/services/pashion/InDigitalService.groovy
+++ grails-app/services/pashion/InDigitalService.groovy
@@ -2,6 +2,8 @@ package pashion
 
 import groovyx.net.http.FromServer
 import groovyx.net.http.ApacheHttpBuilder
+import static groovyx.net.http.HttpBuilder.configure
+
 import grails.transaction.Transactional
 import reactor.spring.context.annotation.*
 
@@ -10,6 +12,50 @@ import reactor.spring.context.annotation.*
 class InDigitalService {
 
     static scope = "singleton"
-    def uri =  'https://sandbox-api.stuart.com'
+    def uri =  'http://indigitalimages.com'
+    def downloadUri = 'http://indigitalimages.com/filemanager/download/sfilate/'
+    
+	String credentials = 'username=pashiontoolapi&password=3jpnb7buy3554t95'
+
+	/*
+		Params name:
+		• catalogueId Catalogue	text	code
+		• seasonId Season	text	code
+		• genderId Gender	text	code
+		• cityId City	text	code
+		• designerId Designer	text	code
+		• categoryId Category	text	code
+		• page Page	number	(usually	in	the	first	call	is	omitted)
+	*/
+	def search(Map params){
+		def action = 'search.do?'
+		
+		def path = "search.do?username=pashiontoolapi&password=3jpnb7buy3554t95"
+
+		def http = ApacheHttpBuilder.configure {
+		    	request.uri = uri
+		}
+		def search = http.get {
+				
+	    		request.uri.path = path
+	    		
+	    		response.success { FromServer from, Object body ->
+	    			log.info "body:"+body
+	        		return body
+	    		}
+		}
+		search
+	}
+	// http://indigitalimages.com/indigital/api/
+	// categories.do?username=api@indigitalimages.com&password=indigitalimages
+	def categories(){	
+
+		def page = configure {
+    		request.uri = 'http://indigitalimages.com/indigital/api/categories.do?username=pashiontoolapi&password=3jpnb7buy3554t95'
+		}.get()
+
+		page
+
+	}
 
 }
