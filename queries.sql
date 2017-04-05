7c5a305f7aa0d7ae88b53076eae8824443c92f24
Gregg Dickson <gregory.d.dickson@gmail.com>
2017-04-05 13:18:56 -0500

    added a sql query for finding samples newly created

--- queries.sql
+++ queries.sql
@@ -7,3 +7,10 @@ update sample_request set shipping_out_id = null;
 update sample_request set shipping_return_id = null;
 delete from shipping_event;
 delete from sample_request;
+
+
+/*
+	Find all samples for a brand and collection creatd after a certain date
+*/
+select * from searchable_item where brand_id = '45' and type_id = '2' 
+and brand_collection_id = '168' and date_created > '2017-04-03 17:33:50';
