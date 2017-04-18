c3ca08ce86d66fb0251a7e689be08c972691d67c
dickmoonpie <mr.richard.moon@gmail.com>
2017-04-18 18:58:45 -0500

    Add restore parameter for instantiation of pub nub instance to help catchup missed messages

--- src/main/webapp/src/services/pubNubService.js
+++ src/main/webapp/src/services/pubNubService.js
@@ -11,6 +11,7 @@ export class PubNubService {
           subscribeKey: "sub-c-dd158aea-b76b-11e6-b38f-02ee2ddab7fe",
           publishKey: "pub-c-b5b66a91-2d36-4cc1-96f3-f33188a8cc73",
           ssl: true
+          , restore: true // automatic re-connect listeners
       });
     }
 
