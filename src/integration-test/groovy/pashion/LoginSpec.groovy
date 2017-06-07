24bf1bce2553320c47d4fe105336fd507c745895
Gregg Dickson <gregory.d.dickson@gmail.com>
2017-06-07 10:59:06 -0500

    start automated testing #567

---
+++ src/integration-test/groovy/pashion/LoginSpec.groovy
@@ -0,0 +1,29 @@
+package pashion
+
+import grails.test.mixin.integration.Integration
+import grails.transaction.*
+
+import spock.lang.*
+import geb.spock.*
+
+/**
+ * See http://www.gebish.org/manual/current/ for more instructions
+ */
+@Integration
+@Rollback
+class LoginSpec extends GebSpec {
+
+    def setup() {
+    }
+
+    def cleanup() {
+    }
+
+    void "test something"() {
+        when:"The home page is visited"
+            go '/'
+
+        then:"The title is correct"
+        	title == "Welcome to Grails"
+    }
+}
