24d9b7dc0b4c61b366eeb96e76067ff0cd0ac652
dickmoonpie <mr.richard.moon@gmail.com>
2017-08-26 13:54:19 -0400

    Fix ability to search in index images for client supplied id

--- grails-app/controllers/pashion/SearchableItemController.groovy
+++ grails-app/controllers/pashion/SearchableItemController.groovy
@@ -93,7 +93,7 @@ class SearchableItemController {
                         keywords.each { ilike('attributes', "%${it}%") }
                         keywords.each { ilike('message', "%${it}%") }
                         // keywords.each { ilike('id'.toString(), "%{it}%")}
-                        keywords.each { ilike('clientid', "%{it}%")}
+                        keywords.each { ilike('clientid', "%${it}%") }
                     }
                 } 
 
@@ -533,7 +533,7 @@ class SearchableItemController {
                 sample.brand = item.brand
                 sample.type = SearchableItemType.get(2)
             }
-            sample.clientID = it.clientID
+            sample.clientid = it.clientid
             sample.sampleType = it.sampleType
             sample.color = it.color
             sample.name = it.name
